830f364f2e3be7c778c1244b5a33f27d899b9612
Пермитин Яков Михайлович <bigjobio@MacBook-Air-Permitin.local>
2021-03-24 12:05:01 +0300

    bigjobio

---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfines <tfines@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/15 18:10:06 by tfines            #+#    #+#             */
+/*   Updated: 2020/11/22 20:53:14 by tfines           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*ret;
+	unsigned	int	i;
+
+	if (!s || !f)
+		return ((void *)0);
+	ret = ft_strdup(s);
+	if (!ret)
+		return ((void *)0);
+	i = 0;
+	while (ret[i])
+	{
+		ret[i] = f(i, ret[i]);
+		i++;
+	}
+	return (ret);
+}
---
+++ cub3d.h
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bigjobio <bigjobio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/14 18:48:01 by bigjobio          #+#    #+#             */
+/*   Updated: 2021/03/23 18:50:36 by bigjobio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "mms/mlx.h"
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <errno.h>
+# include "gnl/get_next_line.h"
+
+typedef struct                  s_list
+{
+    int     r_x;
+    int     r_y;
+    char    *path_to_north;
+    char    *south;
+    char    *path_to_south;
+    char    *west;
+    char    *path_to_west;
+    char    *east;
+    char    *path_to_east;
+    char    *sprite;
+    char    *path_to_sprite;
+    char    *floor;
+    int     floor_red;
+    int     floor_green;
+    int     floor_blue;
+    char    *ceiling;
+    int     ceiling_red;
+    int     ceiling_green;
+    int     ceiling_blue;
+        
+}                               t_list;
+
+
+void    print_error(int numerror);
+void    errors(int  argc, char **argv);
+void    errors2(int  argc, char **argv);
+void    init_flags(t_list *big);
+int     gnl(int  argc, char **argv, t_list *list);
+void    parse_r(t_list *list, char **big);
+void    parse_NO_SO(t_list *list, char **big);
+void    parse_WE_EA(t_list *list, char **big);
+void    parse_S(t_list *list, char **big);
+void    parse_F(char *line, t_list *list);
+void    parse_C(char *line, t_list *list);
+void    parse_1(char *line, t_list *list);
+
+
+#endif
